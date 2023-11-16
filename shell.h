392428de0d0d647edf186067fef19438f4fc5402
TheOrbMaverick <enangita@gmail.com>
2023-11-17 00:12:17 +0100

    changed header files

--- shell.h
+++ shell.h
@@ -4,14 +4,8 @@
 /* Include necessary header files */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-#include <limits.h>
-#include <stdarg.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 
 /* Function Prototypes*/
 void user_prompt(void);
