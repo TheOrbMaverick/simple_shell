908ee3eb529348b44e35a38217ec2288b37c7b65
TheOrbMaverick <enangita@gmail.com>
2023-11-16 17:56:50 +0100

    starting over

--- shell.h
+++ shell.h
@@ -15,20 +15,23 @@
 
 /* Function Prototypes*/
 int main(void);
-// void user_prompt(void);
-// void our_printf(const char *format, ...);
-// char *read_input(void);
-// void execute_input(char *user_input, char *argv[], char **env);
-// char *ourhash_comment(char *input_string);
-// char *simple_getline(FILE *input_stream);
-// void simple_cd(char **args, int nof_args);
-// void simple_env(char **env);
-// int hell_exit(char **args, char *val_put);
-// int func_builtin_commands(char **args, int nof_args, char *val_put, char **env);
-// char *find_file(const char *directories, const char *filename);
-// char *f_path(const char *name);
-// int forward_slash(const char *str);
-// int tokenizer(char *input, char *args[]);
+/*
+void user_prompt(void);
+void our_printf(const char *format, ...);
+char *read_input(void);
+void execute_input(char *user_input, char *argv[], char **env);
+char *ourhash_comment(char *input_string);
+char *simple_getline(FILE *input_stream);
+void simple_cd(char **args, int nof_args);
+void simple_env(char **env);
+int hell_exit(char **args, char *val_put);
+int func_builtin_commands(char **args, int nof_args, char *val_put, char **env);
+char *find_file(const char *directories, const char *filename);
+char *f_path(const char *name);
+int forward_slash(const char *str);
+int tokenizer(char *input, char *args[]);
+*/
+
 
 
 #endif
