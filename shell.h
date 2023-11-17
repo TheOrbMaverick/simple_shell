a01af445a8b8df5aad83462754ebf990edcbd4a9
TheOrbMaverick <enangita@gmail.com>
2023-11-17 16:30:26 +0100

    pid

--- shell.h
+++ shell.h
@@ -2,16 +2,8 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-void display_prompt(void);
-char *read_input(void);
-void execute_command(char *command);
 void my_printf(const char *format, ...);
-char *custom_strtok(char *str, const char *delim);
 
 #endif
